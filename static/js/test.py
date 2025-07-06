8159bc41e7d0200dc5ac5e0fc2d96d4cb3e5f12a
EDMPL <akaedmpl@gmail.com>
2025-07-06 11:54:42 +0700

    obfuscate

--- static/js/test.py
+++
@@ -1,8 +0,0 @@
-           print(f"User input: {user_input}")
-            messages = [
-            {
-                "role": "system",
-                "content": s_p,
-            },
-            {"role": "user", "content": user_input},
-            ]
